1615a0f1a9aa465a04bc93d9fa06fdadc1611cc4
SeigneurLefou <lchamard@proton.me>
2026-06-25 11:21:42 +0200

    feat: start the cpp03 with ex00

---
+++ cpp03/ex00/includes/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap {
+	public:
+		ClapTrap( void );
+		ClapTrap( const ClapTrap &other );
+		ClapTrap &operator=( const ClapTrap &other );
+		~ClapTrap( void );
+		void	attack( const std::string& target );
+		void	takeDamage( unsigned int amount );
+		void	beRepaired( unsigned int amount );
+
+	private:
+		std::string	_name;
+		int			_hit_points;
+		int			_energy;
+		int			_damage;
+};
+
+#endif // CLAPTRAP_HPP
---
+++ cpp03/ex00/src/ClapTrap.cpp
@@ -0,0 +1,65 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap( void ) {
+
+    std::cout << "ClapTrap constructed without name" << std::endl;
+	_name = "nameless";
+	_hit_points = 10;
+	_energy = 10;
+	_damage = 0;
+}
+
+ClapTrap::ClapTrap( std::string name ) {
+
+    std::cout << "ClapTrap constructed" << std::endl;
+	_name = name;
+	_hit_points = 10;
+	_energy = 10;
+	_damage = 0;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other) {
+
+    std::cout << "ClapTrap copied" << std::endl;
+    *this = other;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &other) {
+
+    std::cout << "ClapTraps assigned" << std::endl;
+    return *this;
+}
+
+ClapTrap::~ClapTrap() {
+
+    std::cout << "ClapTraps destroyed" << std::endl;
+}
+
+void	ClapTrap::attack( const std::string& target ) {
+
+	if ( _energy <= 0 ) {
+		std::cout << "ClapTrap " << _name << " has run out of energy." << std::endl;
+	} else if ( _hit_points <= 0 ) {
+		std::cout << "ClapTrap " << _name << " has run out of hit points." << std::endl;
+	} else {
+		std::cout << "ClapTrap " << _name << " attacks " << target ", causing " << _damage << " points of damage!" << std::endl;
+	}
+}
+
+void	ClapTrap::takeDamage( unsigned int amount ) {
+
+		std::cout << "ClapTrap " << _name << " take " << amount << " damages." << std::endl;
+		_hit_points -= amount;
+}
+
+void	ClapTrap::beRepaired( unsigned int amount ) {
+	if ( _energy <= 0 ) {
+		std::cout << "ClapTrap " << _name << " has run out of energy." << std::endl;
+	} else if ( _hit_points <= 0 ) {
+		std::cout << "ClapTrap " << _name << " has run out of hit points." << std::endl;
+	} else {
+		--_energy;
+		_hit_points += amount;
+		std::cout << "ClapTrap " << _name << " has been repaired of " << amount << " HP." << std::endl;
+	}
+}
